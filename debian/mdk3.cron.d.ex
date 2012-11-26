18f238321d723cb4521a6d14fb816fb98bdbcdac
Devon Kearns <dookie@kali.org>
2012-11-26 16:56:48 -0700

    Pre-build commit

--- debian/mdk3.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the mdk3 package
-#
-0 4	* * *	root	[ -x /usr/bin/mdk3_maintenance ] && /usr/bin/mdk3_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(mdk3):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="mdk3" command="/usr/bin/mdk3"
