49db26feaac3064d9edd01e9e3f5f785645c6117
Devon Kearns <dookie@kali.org>
2012-11-26 16:46:55 -0700

    Commit before modifying

---
+++ debian/emacsen-remove.ex
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+# /usr/lib/emacsen-common/packages/remove/mdk3
+
+FLAVOR=$1
+PACKAGE=mdk3
+
+if [ ${FLAVOR} != emacs ]; then
+    if test -x /usr/sbin/install-info-altdir; then
+        echo remove/${PACKAGE}: removing Info links for ${FLAVOR}
+        install-info-altdir --quiet --remove --dirname=${FLAVOR} /usr/share/info/mdk3.info.gz
+    fi
+
+    echo remove/${PACKAGE}: purging byte-compiled files for ${FLAVOR}
+    rm -rf /usr/share/${FLAVOR}/site-lisp/${PACKAGE}
+fi
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(mdk3):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="mdk3" command="/usr/bin/mdk3"
